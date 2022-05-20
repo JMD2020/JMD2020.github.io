b86c3a3818d03c2b8bc1ccaff0e8a5fcebd97ea5
JMD2020 <98502524+JMD2020@users.noreply.github.com>
2022-05-19 23:26:44 -0400

    Add files via upload

---
+++ Portfolio/main.js
@@ -0,0 +1,14 @@
+
+ 
+
+ 
+ 
+ window.console = window.console || function(t) {};
+
+ 
+ 
+ 
+ if (document.location.search.match(/type=embed/gi)) {
+   window.parent.postMessage("resize", "*");
+ }
+
