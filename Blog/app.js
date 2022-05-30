f85c3af8f86f020f570cde1cbcea9b0a5cc1c942
JMD2020 <98502524+JMD2020@users.noreply.github.com>
2022-05-30 09:59:18 -0400

    Update app.js

--- Blog/app.js
+++ Blog/app.js
@@ -8,8 +8,8 @@ const mongoose = require('mongoose');
 
 const homeStartingContent = "This journal, touches on all things coding.";
         
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const aboutContent = " This journal is for my coding notes to help prepare me for a job in the tech industry.";
+
 
 const app = express();
 
@@ -75,10 +75,8 @@ const requestedPostId = req.params.postId;
 app.get("/about", function(req, res){
   res.render("about", {aboutContent: aboutContent});
 });
-//contact page
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
-});
+
+
 
 let port = process.env.PORT;
 if (port == null || port == "") {
