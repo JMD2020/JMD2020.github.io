df5684798e6ca0c24ab4b0f274992b99a4c06d39
JMD2020 <98502524+JMD2020@users.noreply.github.com>
2022-08-16 12:10:38 -0400

    first commit

---
+++ keeper-App/build/precache-manifest.0d3f911750756d770c03ba79db87ade4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae965612f0df5ff9716aa70987f8d925",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4db286bc047176e313b0",
+    "url": "/static/js/2.c6db2bb7.chunk.js"
+  },
+  {
+    "revision": "b45656f326ccf6a39af5",
+    "url": "/static/js/main.1eeb51cc.chunk.js"
+  },
+  {
+    "revision": "10e117344dea3db99e26",
+    "url": "/static/js/runtime-main.b5766938.js"
+  }
+]);
